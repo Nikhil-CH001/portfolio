29b54dc8ea2fa4cfb073463b94c4d6a6262dbc5b
Nikhil-CH001 <nikhilchy001@gmail.com>
2025-03-27 01:53:43 +0545

    src/pages/MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Skills from '../components/skill/skills'
 import Footer from '../components/NavBar&Footer/Footer'
 import NavBar from '../components/NavBar&Footer/NavBar'
 import Contact from '../components/Contact/Contact'
 import Projects from '../components/Project/Projects'
 import Qualification from '../components/Qualification/Qualification'
 import About from '../components/About/About'
+import Skills from '../components/skill/skills'
 
 function Main() {
   return (
