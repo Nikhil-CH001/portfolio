40ca800a4903166f896456df235c08898038e31d
Nikhil <nikhil@Nikhil>
2025-03-27 12:08:15 +0545

    third commit

--- src/components/About/ProfileImage.jsx
+++ src/components/About/ProfileImage.jsx
@@ -1,10 +1,12 @@
+import React from "react";
+import nikhil from "../../../image/profile.jpeg"
 function ProfileImage() {
     return (
       <div className="w-full md:w-1/2 relative flex justify-center">
         <div className="relative z-10">
           <img
             className="rounded-full w-[50%] sm:w-[60%] md:w-[70%] lg:w-[55%] xl:w-[50%] mx-auto outline outline-[0.5rem] sm:outline-[0.7rem] outline-offset-[0.1rem] outline-blue-400/30 shadow-lg shadow-blue-500/50"
-            src="./image/profile.jpeg"
+            src={nikhil}
             alt="Nikhil's Profile"
           />
         </div>
--- src/components/skill/skills.jsx
+++ src/components/skill/skills.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SkillCard from './SkillCard';
-import { skillsData } from './SkillsData';
+import { skillsData } from './skillsData';
 
 function Skills() {
   return (
